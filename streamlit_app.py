3a026e68e55367a01a684012ab202540b6a0cb28
kayuhanbiduk <57620820+kayuhanbiduk@users.noreply.github.com>
2023-10-06 16:38:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 import pandas
-import requests
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -20,5 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
